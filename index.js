e61352e2a6459b58b8feb39515040f75770d7bc1
octisancho <octaviosancho30@gmail.com>
2022-11-23 21:03:26 -0300

    auth & routes

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const userRoute = require('./routes/user');
 const authRoute = require('./routes/auth');
+const productRoute = require('./routes/product');
+const cartRoute = require('./routes/cart');
+const orderRoute = require('./routes/order');
+
 
 dotenv.config();
 
@@ -19,6 +23,10 @@ app.use(express.urlencoded({extended: true}))
 
 app.use('/api/auth', authRoute);
 app.use('/api/users', userRoute);
+app.use('/api/products', productRoute);
+app.use('/api/carts', cartRoute);
+app.use('/api/orders', orderRoute);
+
 
 app.listen(PORT, () => {
   console.log("Server Running");
